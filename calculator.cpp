06e8b2c8e09a322d07ed2e75c7b655672ed034d4
Gramer <gramer3514@gmail.com>
2024-03-24 21:57:52 -0700

    updating

--- calculator.cpp
+++ calculator.cpp
@@ -1,34 +1,49 @@
 /*
  * Calculator.cpp
  *
- *  Date: [Enter date]
- *  Author: [Your Name]
+ *  Date: [3/24]
+ *  Author: [Chris Gramer]
  */
 
 #include <iostream>
 
 using namespace std;
 
-void main()
+int main() //changed to int. Haven't seen void in any of our work
 {
 	char statement[100];
 	int op1, op2;
 	char operation;
-	char answer="Y"
-	while (answer=='y')
+	char answer ='Y'; //added semicolon and the initialized with 'Y' vs "Y"
+
+	while (answer=='y' || answer == 'Y') // can be case sensitive so inserted the or to read capital or lower case y
 	{
 		cout << "Enter expression" <<endl;
 		cin >> op2 >> operation >> op1;
-		if (operation == "+");
-			cout << op1 << " + " << op2 >> " = " << op1 + op2 << endl;
-		if (operation == '-');
-			cout >> op1 << " - " << op2 << " = " << op1 - op2 << endl;
-		if (operation == '*')
-			cout << op1 << " / " << op2 << " = " << op1 * op2 << endl
-		if (operation == '/')
+        //removed semicolon after if statements and added '{ }'
+		if (operation == "+") { 
+            //there was a '>>' when there should have been '<<'
+			cout << op1 << " + " << op2 << " = " << op1 + op2 << endl;
+        }
+
+		if (operation == '-') {
+            //again, there was a '>>' when there should have been '<<'
+            cout << op1 << " - " << op2 << " = " << op1 - op2 << endl;
+        }
+
+    	if (operation == '*') {
+            //added ; to the end of the statement
+			cout << op1 << " / " << op2 << " = " << op1 * op2 << endl;
+        }
+
+		if (operation == '/') {
 			cout << op1 << " * " << op2 << " = " << op1 / op2 << endl;
+        }
 
 		cout << "Do you wish to evaluate another expression? " << endl;
 		cin >> answer;
 	}
+
+    //added return
+    return 0;
 }
